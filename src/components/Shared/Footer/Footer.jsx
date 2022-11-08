79d5a0ea54a28bfdfe7049f9f345e3b1e1f02a0b
kskanak <kskanak26@gmail.com>
2022-11-08 12:36:15 +0600

    footer added

--- src/components/Shared/Footer/Footer.jsx
+++ src/components/Shared/Footer/Footer.jsx
@@ -1,7 +1,54 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import logo from "../../../assets/icon.png";
 
 const Footer = () => {
-  return <div>footer</div>;
+  return (
+    <div>
+      <footer className="px-4 py-8 dark:bg-gray-800 dark:text-gray-400 bg-gradient-to-tl from-teal-500 via-teal-600 to-amber-800">
+        <div className="container  flex flex-wrap items-center justify-center md:justify-between mx-auto space-y-4 ">
+          <div className="flex flex-row pr-3 space-x-4 sm:space-x-8">
+            <div className="flex items-center justify-center flex-shrink-0 w-12 h-12 rounded-full dark:bg-violet-400 md:mr-40">
+              <Link to="/" className="flex items-center text-3xl ">
+                {" "}
+                <img src={logo} alt="" className="h-10" />
+                Interior
+              </Link>
+            </div>
+            <ul className="flex flex-wrap items-center space-x-4 sm:space-x-8 ">
+              <li>
+                <a rel="noopener noreferrer" href="#">
+                  Terms of Use
+                </a>
+              </li>
+              <li>
+                <a rel="noopener noreferrer" href="#">
+                  Privacy
+                </a>
+              </li>
+            </ul>
+          </div>
+          <ul className="flex flex-wrap pl-3 space-x-4 sm:space-x-8">
+            <li>
+              <a rel="noopener noreferrer" href="#">
+                Instagram
+              </a>
+            </li>
+            <li>
+              <a rel="noopener noreferrer" href="#">
+                Facebook
+              </a>
+            </li>
+            <li>
+              <a rel="noopener noreferrer" href="#">
+                Twitter
+              </a>
+            </li>
+          </ul>
+        </div>
+      </footer>
+    </div>
+  );
 };
 
 export default Footer;
